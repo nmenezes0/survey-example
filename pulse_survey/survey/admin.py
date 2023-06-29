689b018e2983bc450bc3ad4146de7a7063b657ff
George Burton <g.e.c.burton@gmail.com>
2023-06-29 08:38:27 +0100

    added an ordering to the ResultAdmin

--- pulse_survey/survey/admin.py
+++ pulse_survey/survey/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from pulse_survey.survey.models import Result, Feedback
 
 
+class ResultAdmin(admin.ModelAdmin):
+    ordering = ["-created_at"]  # Order by created_at in descending order
+
+
 admin.site.register(Result)
 admin.site.register(Feedback)
