4f28e15f109313129dedbe66b8e5461cb9811e23
George Burton <g.e.c.burton@gmail.com>
2023-06-29 08:56:00 +0100

    added new test_email_saved_as_lower_case

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,3 +8,8 @@ class FeedbackModelTest(TestCase):
         feedback = models.Feedback(email="Test@Example.Com", content="Some feedback")
         feedback.save()
         self.assertEqual(feedback.content, "Some feedback")
+
+    def test_email_saved_as_lower_case(self):
+        feedback = models.Feedback(email="Test@Example.Com", content="Some feedback")
+        feedback.save()
+        self.assertEqual(feedback.email, "test@example.com")
