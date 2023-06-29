0e1849f170975718a482f700073973887cf5704f
George Burton <g.e.c.burton@gmail.com>
2023-06-29 08:43:18 +0100

    now validating cabinet office email address

--- pulse_survey/survey/forms.py
+++ pulse_survey/survey/forms.py
@@ -3,8 +3,13 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 
-def is_cabinet_office_email(email_address):
-    # TODO - update to raise a ValidationError for non-Cabinet Office emails
+def is_cabinet_office_email(email_address: str) -> bool:
+    if not email_address.endswith("@cabinetoffice.gov.uk"):
+        raise ValidationError("a cabinet office email must be used")
+    # TODO: we should check that this is a cabinet office email that really exists,
+    # not just one with the right domain
+    
+    # TODO: this is breaking tests, this will be fixed in a separate PR branching from this one
     return True
     
 
